069a4adbedd37e261ec91eaee4ae8d6279577485
MakKou96 <155227005+MakKou96@users.noreply.github.com>
2026-09-24 12:08:22 +0300

    Add program to calculate product of digits in a number

---
+++ HW3/task13.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int x, num_1, num_2, num_3;
+	scanf("%d", &x);
+	num_1 = x / 100;
+	num_2 = (x % 100) / 10;
+	num_3 = x % 10;
+	printf("%d", num_1 * num_2 * num_3);
+	
+	return 0;
+}
+	
